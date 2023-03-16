0a0eb8ca57f06ce1ecc3f985959e668114f0d009
John Dunlap <john.david.dunlap@gmail.com>
2023-03-15 21:37:23 -0400

    Added an EntryPoint interface to support issue #19

---
+++ src/main/java/pro/johndunlap/getopt/EntryPoint.java
@@ -0,0 +1,21 @@
+package pro.johndunlap.getopt;
+
+/**
+ * This allows a single line code in the normal public static void main(String[]) method to invoke a statically typed
+ * main method inside the object being bound to. That method will not be invoked unless there are no validation errors.
+ * However, when it is invoked the developer has all a hydrated object containing everything they need to start writing
+ * code and can proceed as if it were the real main method. Getopt will automatically invoke this method when it
+ * notices that the object implements it. This will work exceptionally well in multi-layered command line interfaces
+ * which can have separate entry points for each of the types that they support. The correct entrypoint is automatically
+ * invoked based on the arguments which were passed on the command line.
+ * @param <T> the type of the object which will be passed to the main method.
+ */
+public interface EntryPoint<T> {
+    /**
+     * This method will be invoked with a hydrated object containing all arguments which were passed on the
+     * command line. This method will not be invoked unless there are no validation errors.
+     * @param args the hydrated object
+     * @return The exit code. Zero should be returned on success and non-zero otherwise
+     */
+    int main(T args);
+}
